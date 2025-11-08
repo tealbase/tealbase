c04e404c4a9cd7e3878cd34c1cfdf9a21875568a
Ekjot Singh <ekjotmakhija@gmail.com>
2025-11-09 00:44:47 +0530

    added Companies to data

---
+++ apps/www/data/Companies.tsx
@@ -0,0 +1,105 @@
+type Companies = {
+  name: string
+  image: string
+}
+
+const data: Companies[] = [
+  {
+    name: 'wells-fargo',
+    image: '/images/company/companies-using-tealbase/wells-fargo.png',
+  },
+  {
+    name: 'under-armour',
+    image: '/images/company/companies-using-tealbase/under-armour.png',
+  },
+  {
+    name: 'audi-logo',
+    image: '/images/company/companies-using-tealbase/audi-logo.png',
+  },
+  {
+    name: 'capitalone',
+    image: '/images/company/companies-using-tealbase/capitalone.png',
+  },
+  {
+    name: 'coinbase',
+    image: '/images/company/companies-using-tealbase/coinbase.png',
+  },
+  {
+    name: 'facebook',
+    image: '/images/company/companies-using-tealbase/facebook.png',
+  },
+  {
+    name: 'github',
+    image: '/images/company/companies-using-tealbase/github.png',
+  },
+  {
+    name: 'google',
+    image: '/images/company/companies-using-tealbase/google.png',
+  },
+  {
+    name: 'gsk',
+    image: '/images/company/companies-using-tealbase/gsk.png',
+  },
+  {
+    name: 'hewlett-packard',
+    image: '/images/company/companies-using-tealbase/hewlett-packard.png',
+  },
+  {
+    name: 'hubspot',
+    image: '/images/company/companies-using-tealbase/hubspot.png',
+  },
+  {
+    name: 'ibm',
+    image: '/images/company/companies-using-tealbase/ibm.png',
+  },
+  {
+    name: 'instagram',
+    image: '/images/company/companies-using-tealbase/instagram.png',
+  },
+  {
+    name: 'linkedin',
+    image: '/images/company/companies-using-tealbase/linkedin.png',
+  },
+  {
+    name: 'microsoft',
+    image: '/images/company/companies-using-tealbase/microsoft.png',
+  },
+  {
+    name: 'netflix',
+    image: '/images/company/companies-using-tealbase/netflix.png',
+  },
+  {
+    name: 'notion',
+    image: '/images/company/companies-using-tealbase/notion.png',
+  },
+  {
+    name: 'red-hat',
+    image: '/images/company/companies-using-tealbase/red-hat.png',
+  },
+  {
+    name: 'robinhood',
+    image: '/images/company/companies-using-tealbase/robinhood.png',
+  },
+  {
+    name: 'salesforce',
+    image: '/images/company/companies-using-tealbase/salesforce.png',
+  },
+  {
+    name: 'santander',
+    image: '/images/company/companies-using-tealbase/santander.png',
+  },
+  {
+    name: 'shopify',
+    image: '/images/company/companies-using-tealbase/shopify.png',
+  },
+  {
+    name: 'squarespace',
+    image: '/images/company/companies-using-tealbase/squarespace.png',
+  },
+  {
+    name: 'twitter',
+    image: '/images/company/companies-using-tealbase/twitter.png',
+  },
+]
+
+export default data

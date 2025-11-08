e93fbcb6c317d1fd51eb0043a0d002e6a3ae32e4
Ekjot Singh <ekjotmakhija@gmail.com>
2025-11-09 00:45:05 +0530

    added Comparisons to data

---
+++ apps/www/data/Comparisons.tsx
@@ -0,0 +1,22 @@
+export const data = {
+  label: 'Compare Tealbase',
+  comparisons: [
+    {
+      text: 'Tealbase vs Firebase',
+      description: '',
+      url: '/alternatives/tealbase-vs-firebase',
+    },
+    {
+      text: 'Tealbase vs Heroku Postgres',
+      description: '',
+      url: '/alternatives/tealbase-vs-heroku-postgres',
+    },
+    {
+      text: 'Tealbase vs Auth0',
+      description: '',
+      url: '/alternatives/tealbase-vs-auth0',
+    },
+  ],
+}
+
+export default data
